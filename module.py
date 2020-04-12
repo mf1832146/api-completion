4847369d56aa71cb22bc9300bd5c6c689e2a9bd7
tangze <mf1832146@smail.nju.edu.cn>
2020-04-12 15:37:37 +0800

    [MOD] add my model

--- module.py
+++ module.py
@@ -30,7 +30,7 @@ class MyModel(nn.Module):
         self.linear = nn.Linear(in_features=hidden_size,
                                 out_features=api_emb_dim)
         if class_attn:
-            self.class_attn = ClassAttn(class_emb_dim, hidden_size, dropout)
+            self.class_attn = Class2ClassAttn(class_emb_dim, hidden_size, dropout)
         else:
             self.class_attn = None
         self.dropout = nn.Dropout(dropout)
@@ -50,24 +50,22 @@ class MyModel(nn.Module):
 
         class_emb = self.class_emb_layer(class_seq)
         api_emb = self.api_emb_layer(api_seq)
-        class_emb = class_emb.view(batch_size * max_class_len, -1)
-        class_emb = class_emb.unsqueeze(1).repeat(1, max_api_len, 1)
-        api_emb = torch.cat((class_emb, api_emb), dim=-1) * math.sqrt(self.class_emb_dim + self.api_emb_dim)
+        api_class_emb = class_emb.view(batch_size * max_class_len, -1)
+        api_class_emb = api_class_emb.unsqueeze(1).repeat(1, max_api_len, 1)
+        api_emb = torch.cat((api_class_emb, api_emb), dim=-1) * math.sqrt(self.class_emb_dim + self.api_emb_dim)
         class_emb = class_emb * math.sqrt(self.class_emb_dim)
         candidate_api_emb = self.api_emb_layer(candidate_api_seq) * math.sqrt(self.api_emb_dim)
 
         class_output, _ = self.class_rnn(api_emb)
         # batch_size*max_len, max_len, max hidden_size
-        if self.class_attn is not None:
-            mask = api_seq == 0
-            class_output = self.dropout(class_output)
-            class_emb = class_emb[range(len(hole_loc)), hole_loc]
-            class_emb = class_emb.repeat(max_class_len, 1)
-            class_output = self.class_attn(class_emb, class_output, mask)
-        else:
-            class_output = class_output[range(len(api_len)), api_len]
-
+        class_output = class_output[range(len(api_len)), api_len]
         class_output = class_output.view(batch_size, max_class_len, -1)
+        if self.class_attn is not None:
+            mask = class_seq == 0
+            target_class_emb = class_emb[range(len(hole_loc)), hole_loc]
+            scores = self.class_attn(class_emb, target_class_emb, mask)
+            scores = scores.unsqueeze(2)
+            class_output = torch.mul(scores, class_output)
 
         class_len = torch.sum(class_seq != 0, dim=1)
         _, idx_sort = torch.sort(class_len, dim=0, descending=True)
@@ -148,6 +146,32 @@ class APIHelper(nn.Module):
         return output
 
 
+class Class2ClassAttn(nn.Module):
+    def __init__(self, emb_dim, hidden_size, dropout):
+        super(Class2ClassAttn, self).__init__()
+        self.emb_dim = emb_dim
+        self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(dropout)
+        self.W_input = nn.Linear(self.emb_dim + self.emb_dim, 1)
+
+    def forward(self, x, target_class_emb, mask=None):
+        """
+        :param class_emb: [batch_size, max_class_len, emb_dim]
+        :param target_class_emb: [batch_size, emb_dim]
+        :param mask:
+        :return:
+        """
+        batch_size, max_class_len, _ = x.size()
+        target_class_emb = target_class_emb.unsqueeze(1).repeat(1, max_class_len, 1)
+        query = torch.cat((target_class_emb, x), dim=-1)
+        query = query.view(-1, self.emb_dim + self.emb_dim)
+        scores = F.sigmoid(self.W_input(query))
+        scores = self.dropout(scores)
+        scores = scores.view(batch_size, max_class_len)
+        scores = scores.masked_fill(mask, 0)
+        return scores
+
+
 class ClassAttn(nn.Module):
     def __init__(self, emb_dim, hidden_size, dropout):
         super(ClassAttn, self).__init__()
@@ -171,7 +195,7 @@ class ClassAttn(nn.Module):
         scores = torch.bmm(query, h.transpose(1, 2)) / math.sqrt(max_len)
         scores = scores.squeeze(1)
         if mask is not None:
-            scores = scores.masked_fill(mask == 0, -1e9)
+            scores = scores.masked_fill(mask, -1e9)
         p_attn = F.softmax(scores, dim=-1)
         p_attn = self.dropout(p_attn)
 
